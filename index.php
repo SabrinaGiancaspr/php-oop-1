c5b2f5929d53a54c2dc983b4f4e894784fe06733
sabri <sabrinatwitter6@gmail.com>
2023-11-28 16:00:22 +0100

    add foreach

--- index.php
+++ index.php
@@ -39,12 +39,37 @@ class Production{
 $emma_puzza = new Production("Emma puzza", "Italiano", 9.5);
 $rina_profuma = new Production("Rina", "Inglese", 10);
 
+$movies = [$emma_puzza, $rina_profuma];
 // Stampare le informazioni dell'oggetto
-echo "Titolo: " . $emma_puzza->title . "<br>";
-echo "Lingua: " . $emma_puzza->language . "<br>";
-echo "Rating: " . $emma_puzza->rating . "<br>";
+// echo "Titolo: " . $emma_puzza->title . "<br>";
+// echo "Lingua: " . $emma_puzza->language . "<br>";
+// echo "Rating: " . $emma_puzza->rating . "<br>";
 
 // Stampare le informazioni dell'oggetto
-echo "Titolo: " . $rina_profuma->title . "<br>";
-echo "Lingua: " . $rina_profuma->language . "<br>";
-echo "Rating: " . $rina_profuma->rating . "<br>";
+// echo "Titolo: " . $rina_profuma->title . "<br>";
+// echo "Lingua: " . $rina_profuma->language . "<br>";
+// echo "Rating: " . $rina_profuma->rating . "<br>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php 
+            foreach($movies as $film){
+                ?>
+                <li><?php echo $film->getTitle() ?></li>
+                <li><?php echo $film-> getLanguage()?></li>
+                <li><?php echo $film->getRating()?> </li>
+                <?php
+            }
+        ?>
+        
+    </ul>
+</body>
+</html>
