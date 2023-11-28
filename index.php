c6bb6e8d001b46cdc44037d4fb5b46803636a11c
sabri <sabrinatwitter6@gmail.com>
2023-11-28 16:14:32 +0100

    add table

--- index.php
+++ index.php
@@ -56,20 +56,34 @@ $movies = [$emma_puzza, $rina_profuma];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        <?php 
-            foreach($movies as $film){
-                ?>
-                <li><?php echo $film->getTitle() ?></li>
-                <li><?php echo $film-> getLanguage()?></li>
-                <li><?php echo $film->getRating()?> </li>
-                <?php
-            }
+<div class="container">
+    <table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Films</th>
+            <th scope="col">Title</th>
+            <th scope="col">Language</th>
+            <th scope="col">Rating</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($movies as $i => $film){
+            ?>
+            <tr>
+                <th scope="row"><?php echo $i + 1 ?></th>
+                <td><?php echo $film->title ?></td>
+                <td><?php echo $film->language ?></td>
+                <td><?php echo $film->rating ?></td>
+            </tr>
+            <?php
+        }
         ?>
-        
-    </ul>
+    </tbody>
+    </table>
+</div>
 </body>
 </html>
